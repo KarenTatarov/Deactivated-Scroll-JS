7ded9fd6159cd0cc798b95e7b5237e09f08930e6
KarenTatarov <karenatatarov@gmail.com>
2019-11-28 02:05:34 +0300

    Created script Native JS

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(function() {
-    $('#button').click(function() {
-        if($('body').css('overflow') == 'auto') {
-            $('body').css('overflow', 'hidden');
-        }else {
-            $('body').css('overflow', 'auto');
-        }
-    });
-});
+let noscroll;
+button.onclick = function() {
+    if(noscroll) { 
+        document.body.style.overflow = 'auto';
+        document.body.style.paddingRight = "0";
+        noscroll = false
+    } else {
+        document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = "17px";
+        noscroll = true
+    }
+}
